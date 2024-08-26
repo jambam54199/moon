67f2c8818882580f3c66ef218a77b411aa249320
ennis <eunjin_10@naver.com>
2024-08-26 15:22:58 +0900

    관리자 계정 post, comment 추가
    관리자 계정에서 post, comment 추가했고, 관리자 페이지에서 확인 가능합니다.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from posts.models import Post, Comment
 
 # Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "review",
+        "short_comment",
+        "created",
+    ]
+    inlines = [
+        CommentInline,
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+        "created",
+    ]
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey("users.User", # 앱이름.모델이름
+                             verbose_name = "작성자",
+                             on_delete = models.CASCADE) # 작성자 데이터 삭제되면 Post도 같이 삭제
+    review = models.TextField("리뷰", blank = True)
+    short_comment = models.CharField("한줄평", max_length = 50)
+    created = models.DateTimeField("작성일시", auto_now_add = True)
+
+class Comment(models.Model):
+    user = models.ForeignKey("users.User",
+                             verbose_name = "작성자",
+                             on_delete = models.CASCADE)
+    post = models.ForeignKey(Post,
+                             verbose_name = "포스트",
+                             on_delete = models.CASCADE)
+    content = models.TextField("댓글 내용")
+    created = models.DateTimeField("작성일시", auto_now_add = True)
+    
+
