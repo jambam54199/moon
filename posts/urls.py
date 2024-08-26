6879852afab5d33fb127263519cb5252c096ae72
ennis <eunjin_10@naver.com>
2024-08-26 16:03:36 +0900

    base.html 추가 및 app_name 추가
    base.html 추가하고
    post_url에 app_name 추가했습니다.
    runserver 확인했습니다.
    중요 변경사항 없습니다.

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from posts import views
 
+app_name = "posts"
 urlpatterns = [
-    path("feeds/", views.feeds),
+    path("feeds/", views.feeds, name = "post_feeds"),
 ]
