b0501d7ae217d395efc87f8b75e609373987116d
HGaYeoung <rkdud1113@naver.com>
2024-08-27 09:33:42 +0900

    영화 앱, templstes/movies 수정
    movies template 추가, movies/models, urls, views.py 수정했습니다.

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from movies.models import movies_post
+
 
 # Create your views here.
-def movies(request):
-    return render(request, "movies/movies.html")
+def movies_list(request):
+    posts = movies_post.objects.all()
+    context = {"posts": posts}
+    return render(request, "movies/movies.html", context)
+
+
+def movies_datail(request, pk):
+    post = movies_post.objects.get(id=pk)
+
+    context = {"post": post}
+
+    return render(request, "movies/movies_detail.html", context)
