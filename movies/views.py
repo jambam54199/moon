f535588bfaf56273c1559b50986af551ae5790c5
HGaYeoung <rkdud1113@naver.com>
2024-08-27 16:26:27 +0900

    movies/templstes 수정
    명대사, 랜덤이지미 출력 코드 추가

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,24 @@
 from django.shortcuts import render
 from movies.models import movies_post
-<<<<<<< HEAD
-=======
 from posts.models import Post, Comment
->>>>>>> movies
 
 # Create your views here.
 def movies_list(request):
-    posts = movies_post.objects.all()
-    context = {"posts": posts}
+    m_posts = movies_post.objects.all()
+    line = movies_post.objects.get()
+    context = {
+        "m_posts": m_posts,
+        "line" : line,
+        }
     return render(request, "movies/movies.html", context)
 
 
 def movies_detail(request, pk):
-<<<<<<< HEAD
-    post = movies_post.objects.get(id=pk)
-
-    context = {"post": post}
-=======
     m_post = movies_post.objects.get(id=pk)
-    p_post = Post.objects.all()
->>>>>>> movies
+    posts = Post.objects.all()
 
     context = {
-        "m_post": m_post
+        "m_post": m_post,
+        "posts" : posts,
         }
     return render(request, "movies/movies_detail.html", context)
