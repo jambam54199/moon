ab1a47534bbd66baa7550d782c89bb928869d044
ennis <eunjin_10@naver.com>
2024-08-27 14:07:42 +0900

    뷰 연결 커밋
    개인상세-개인목록 연결
    영화상세-영화목록 연결
    영화상세에서 글작성 연결 / 글 작성기능은 구현중에 있습니다.

--- moon/settings.py
+++ moon/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
