08a8c436b8134efa5f43bba8922965a639d62eee
HGaYeoung <rkdud1113@naver.com>
2024-08-28 20:11:05 +0900

    templste/movie 수정, movies.view.py 수정
    detail, serach, movies.html 수정

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from movies.models import movies_post
+from movies.models import movies_post, FamousLine
 
 # Register your models here.
 @admin.register(movies_post)
 class postAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(FamousLine)
+class LineAdmin(admin.ModelAdmin):
     pass
--- movies/views.py
+++ movies/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import render
-from movies.models import movies_post
+from movies.models import movies_post, FamousLine
 from posts.models import Post
 import random
 
 
 # Create your views here.
 def movies_list(request):
-    m_posts = movies_post.objects.all()
-    line = movies_post.objects.all()
+    m_posts = movies_post.objects.all().order_by("-id")
+    line = FamousLine.objects.all()
 
-    ran = random.choice(line)
+    line_ran = random.choice(line)
+    image_ran1 = random.choice(m_posts)
+    image_ran2 = random.choice(m_posts)
 
     context = {
         "m_posts": m_posts,
         "line": line,
-        "ran": ran,
+        "line_ran": line_ran,
+        "image_ran1" : image_ran1,
+        "image_ran2" : image_ran2,
     }
     return render(request, "movies/movies.html", context)
 
 
 def movies_detail(request, pk):
     m_post = movies_post.objects.get(id=pk)
-<<<<<<< HEAD
 
     # 영화 관련 글 목록 조회
     posts = m_post.post_set.all().order_by("-id")
-=======
-    posts = Post.objects.all().order_by("-id")
->>>>>>> parent of fbe2387 (영화 상세 페이지 한줄평 추가)
+
+    # 영화 별점
 
     context = {
         "m_post": m_post,
